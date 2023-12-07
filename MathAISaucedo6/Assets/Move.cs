392e6f135ece7ab280153211e39f3fc21db5bb73
MathSauc3 <732584@student.dusd.net>
2023-12-07 13:46:02 -0800

    Vectors P2

---
+++ MathAISaucedo6/Assets/Move.cs
@@ -0,0 +1,21 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+    public GameObject goal;
+    Vector3 direction;
+    float speed = 0.5f;
+
+    void Start()
+    {
+        direction = goal.transform.position - transform.position;
+    }
+
+    private void LateUpdate()
+    {
+        Vector3 velocity = direction.normalized * speed * Time.deltaTime;
+        this.transform.position = this.transform.position + velocity;
+
+    }
+}
